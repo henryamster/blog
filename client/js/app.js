e9d2956331ffdcc5f54f86cc2f5c6da3060fdabe
henry fritz <henryamsterfritz@gmail.com>
2017-05-18 00:51:32 +0000

    preparing to dynamically load posts

--- client/js/app.js
+++ client/js/app.js
@@ -12,29 +12,42 @@
   };
   firebase.initializeApp(config);
   
-  var title = document.getElementById("title");
-  var content = document.getElementById("content");
-  var dbRef = firebase.database().ref().child('title');
-  var dbRef2 = firebase.database().ref().child('content');
+  //var title = document.getElementById("title").value;
+  //var content = document.getElementById("content").value;
+  //var dbRef = firebase.database().ref().child('blogposts');
+ // var dbRef2 = firebase.database().ref().child('content');
   //dbRef.on('value', snap => title.innerText = snap.val());
   //dbRef2.on('value', snap => content.innerText = snap.val());
   var submitBtn = document.getElementById("submit");
-  var file= document.getElementById("file");
- //submitBtn.onclick(submitBlogPost(title, content));
-
-
-//old submit function 
-function submitBlogPost(title, content, file){
-   firebase.database().ref('blogposts/').set(
-   {
-    file: file,
-    title: title,
-    content: content,
-    date: Date.now(),
-   } )
+ // var file= document.getElementById("file").value;
+ if (submitBtn)
+ {submitBtn.addEventListener("click", function(){submitBlogPost()});
+ }
+ 
  
  
  //new submit function
+ function submitBlogPost(){
+  
+   var title = document.getElementById("title");
+  var content = document.getElementById("content");
+  var file= document.getElementById("file");
+  
+  console.log(title.value + "LOL" + content.value + file.value + Date.now());
+  var blogPost = {
+   title: title.value,
+   content: content.value,
+   file: file.value,
+   date: Date.now()
+  };
+  // return a key for individual post
+  var newBlogKey = firebase.database().ref().child('blogposts').push().key;
+  // place in array in case multiple users/ new functionality are added in the future.
+  var updates = {};
+  updates['blogposts/' + newBlogKey] = blogPost;
+  //return updates to database
+  return firebase.database().ref().update(updates);
+ }
  
 
-}
+
